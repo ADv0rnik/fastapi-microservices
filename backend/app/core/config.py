66bff1f6f9e63b7d8c999ef93446e29cbe864793
ADv0rnik <aadvornik@gmail.com>
2023-11-16 11:39:29 +0300

    Init project

---
+++ backend/app/core/config.py
@@ -0,0 +1,33 @@
+import os
+
+from pathlib import Path
+from typing import List
+
+from pydantic import AnyHttpUrl
+from pydantic_settings import BaseSettings
+
+
+BASE_DIR = (Path(__file__) / ".." / ".." / ".." / "..").resolve()
+ENV_PATH = os.path.join(BASE_DIR, ".env")
+
+
+class Settings(BaseSettings):
+    API_VERSION: str = "/api/v1"
+    PROJECT_NAME: str
+    PROJECT_VERSION: str
+    PROJECT_HOST: str
+    PROJECT_PORT: int
+
+    ALLOWED_ORIGIN: List[AnyHttpUrl] = [
+        "http://localhost",
+        "http://127.0.0.1",
+        "http://0.0.0.0"
+    ]
+
+    class Config:
+        env_file = ENV_PATH
+        env_file_encoding = "utf-8"
+        case_sensitive = True
+
+
+settings = Settings()
---
+++ scripts/start_app.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# shellcheck disable=SC2164
+cd .. &&  cd backend/
+
+python main.py
